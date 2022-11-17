83d18e43f5ce1a809765ba7600c9ca9a9430cf4e
RomanZlobin <rvzlobin@gmail.com>
2022-11-17 16:04:55 +0600

    fixed dategenerator and test

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
@@ -5,6 +6,8 @@ import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -14,28 +17,25 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class AppCardDeliveryTest {
 
-
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
     @Test
     void shouldDeliveryCard() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_MONTH, +4);
-        SimpleDateFormat today = new SimpleDateFormat("dd.MM.yyyy");
-        String expectedDate = today.format(calendar.getTime());
+        String planningDate = generateDate(4);
 
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] .input__control").setValue("Новосибирск");
-        $("[data-test-id='date'] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").sendKeys(Keys.COMMAND + "A");
-        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(expectedDate);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(planningDate);
         $("[data-test-id='name'] .input__control").setValue("Николай Римский-Корсаков");
         $("[data-test-id='phone'] .input__control").setValue("+79529991111");
         $("span[class='checkbox__box']").click();
         $("span[class='button__text']").click();
-        $("div[class='notification__content']")
-                .shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
 }
